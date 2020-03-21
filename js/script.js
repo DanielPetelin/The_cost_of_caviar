a246abc5a2aca2e619d7002c3419610aac2f3211
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-21 17:36:29 +0300

    add fun

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('count').onclick = function() {
+    // переменная для черной икры в кил
+    var your_b = document.getElementById('kil_black').value;
+    // переменная для красной икры в кил
+    var your_r = document.getElementById('kil_red').value;
+    // 45000 руб за киллограм чёрной икры
+    var kl_b = 45000;
+    // 5000 руб за киллограм красной
+    var kl_r = 5000;
+    // стоимость чёрной
+    var buy_b = your_b * kl_b
+    // стоимость красной
+    var buy_r = your_r * kl_r
+    // общая стоимость
+    var result = buy_b + buy_r
+
+    // выводим оющую стоимость по id адресу на страницу
+    document.getElementById('out').innerHTML = result;
+};
